91a7f9c73a300df3588393d653e398e70d1188ef
HyunJaeyeon <bomul0109@gmail.com>
2023-12-06 21:35:17 +0900

    refactor: dispatch와 selector에 type을 지정해줌

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,3 +6,9 @@ const store = configureStore({
     user: userReducer,
   },
 });
+
+// useSelector 사용시 타입으로 사용하기 위함
+export type RootState = ReturnType<typeof store.getState>;
+// useDispatch를 좀 더 명확하게 사용하기 위함
+export type AppDispatch = typeof store.dispatch;
+export default store;
